9f7509f3ce4ff3ca9993fc96fae1aabf9104d425
Gabriel Lingnau <gabriellingnau@MacBook-Air-de-Gabriel.local>
2026-02-25 09:53:22 -0400

    chore: add route error and loading boundaries

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { LandingPage } from "@/templates/landing-page";
 
-export default function Page() {
-  return <LandingPage />;
+async function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export default async function Page() {
+
+  await delay(10 * 1000)
+  throw new Error("Erro simulado para teste");
+  return <LandingPage/>;
 }
